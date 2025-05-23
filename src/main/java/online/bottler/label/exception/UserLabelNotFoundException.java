6581bf8fb34b0d1b6c66ad58a61707b7300beb03
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-05-24 01:52:25 +0900

    refactor: 기존 exception 삭제

--- src/main/java/online/bottler/label/exception/UserLabelNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package online.bottler.label.exception;
-
-public class UserLabelNotFoundException extends RuntimeException {
-    public UserLabelNotFoundException(String message) {
-        super(message);
-    }
-
-}
