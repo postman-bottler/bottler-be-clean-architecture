0aaecc010c548a06e0e20414a230a8d7bd0c54c0
l2yuPa <jeungwon28@gmail.com>
2025-06-09 14:39:16 +0900

    refactor: UserKeywordService 트랜잭션 설정

--- src/main/java/online/bottler/letter/application/UserKeywordService.java
+++ src/main/java/online/bottler/letter/application/UserKeywordService.java
@@ -8,6 +8,7 @@ import online.bottler.letter.application.port.out.UserKeywordPersistencePort;
 import online.bottler.letter.application.response.UserKeywordResponse;
 import online.bottler.letter.domain.UserKeyword;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +16,13 @@ public class UserKeywordService implements UserKeywordUseCase {
 
     private final UserKeywordPersistencePort userKeywordPersistencePort;
 
+    @Transactional
     @Override
     public void create(UserKeywordCommand command) {
         userKeywordPersistencePort.replaceKeywordsByUserId(command.toDomainList(), command.userId());
     }
 
+    @Transactional(readOnly = true)
     @Override
     public UserKeywordResponse getKeywords(Long userId) {
         List<UserKeyword> userKeywords = userKeywordPersistencePort.loadUserKeywords(userId);
