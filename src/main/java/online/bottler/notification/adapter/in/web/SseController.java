97a936c1063ac83cf1348af36d1b5ec17354fd81
hgh1472 <hgh1472@naver.com>
2025-06-30 12:55:40 +0900

    feature: SSE 기능 Swagger 적용

--- src/main/java/online/bottler/notification/adapter/in/web/SseController.java
+++ src/main/java/online/bottler/notification/adapter/in/web/SseController.java
@@ -1,5 +1,7 @@
 package online.bottler.notification.adapter.in.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import online.bottler.auth.CustomUserDetails;
 import online.bottler.notification.application.SseService;
@@ -11,10 +13,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "SSE API", description = "접속 사용자는 SSE 연결을 통해 알림 수신 가능")
 public class SseController {
 
     private final SseService sseService;
 
+    @Operation(summary = "SSE 연결", description = "접속한 사용자의 SSE 연결")
     @GetMapping(value = "/sse/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter connect(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return sseService.connect(customUserDetails.getUserId().toString());
