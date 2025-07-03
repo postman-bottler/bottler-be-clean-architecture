27489b92590bd335a607c06bedaaefe774e415c1
l2yuPa <jeungwon28@gmail.com>
2025-07-03 18:12:46 +0900

    feat: LetterBoxType

---
+++ src/main/java/online/bottler/letter/adapter/out/persistence/entity/LetterBoxTypeEntity.java
@@ -0,0 +1,45 @@
+package online.bottler.letter.adapter.out.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import online.bottler.letter.domain.BoxType;
+import online.bottler.letter.domain.LetterBoxType;
+import online.bottler.letter.domain.LetterType;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LetterBoxTypeEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "letter_type", nullable = false)
+    private LetterType letterType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "box_type", nullable = false)
+    private BoxType boxType;
+
+    @Builder
+    public LetterBoxTypeEntity(LetterType letterType, BoxType boxType) {
+        this.letterType = letterType;
+        this.boxType = boxType;
+    }
+
+    public static LetterBoxTypeEntity from(LetterBoxType letterBoxType) {
+        return LetterBoxTypeEntity.builder()
+                .letterType(letterBoxType.getLetterType())
+                .boxType(letterBoxType.getBoxType())
+                .build();
+    }
+
+    public LetterBoxType toDomain() {
+        return LetterBoxType.of(
+                letterType,
+                boxType
+        );
+    }
+}
---
+++ src/main/java/online/bottler/letter/domain/LetterBoxType.java
@@ -0,0 +1,27 @@
+package online.bottler.letter.domain;
+
+import lombok.Getter;
+
+@Getter
+public class LetterBoxType {
+
+    private final LetterType letterType;
+    private final BoxType boxType;
+
+    private LetterBoxType(LetterType letterType, BoxType boxType) {
+        this.letterType = letterType;
+        this.boxType = boxType;
+    }
+
+    public static LetterBoxType of(LetterType letterType, BoxType boxType) {
+        return new LetterBoxType(letterType, boxType);
+    }
+
+    public static LetterBoxType from(String letterType, String boxType) {
+        return LetterBoxType.of(LetterType.valueOf(letterType), BoxType.valueOf(boxType));
+    }
+
+    public boolean isValid() {
+        return this.letterType != null && this.boxType != null;
+    }
+}
