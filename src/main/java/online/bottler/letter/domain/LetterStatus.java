d2e54720be3ab48dc96ad4c2f2ed50ba7a98e044
l2yuPa <jeungwon28@gmail.com>
2025-07-04 16:29:17 +0900

    refactor: Make LetterStatus Use this

--- src/main/java/online/bottler/letter/domain/LetterStatus.java
+++ src/main/java/online/bottler/letter/domain/LetterStatus.java
@@ -4,17 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum LetterStatus {
-    OPEN("OPEN"),
-    BLOCKED("BLOCKED"),
-    DELETED("DELETED");
-
-    private final String status;
-
-    LetterStatus(String status) {
-        this.status = status;
-    }
+    OPEN,
+    BLOCKED,
+    DELETED;
 
     public boolean isOpen() {
-        return status.equals("OPEN");
+        return this == OPEN;
     }
 }
