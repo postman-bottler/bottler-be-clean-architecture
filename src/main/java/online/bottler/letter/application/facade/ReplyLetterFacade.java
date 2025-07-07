9778e388e1acdf5b0f63b8ab3c431675667613d9
l2yuPa <jeungwon28@gmail.com>
2025-07-07 13:28:56 +0900

    refactor: Change LetterBox Save Method In LetterWithKeywordsFacade And ReplyLetterFacade

--- src/main/java/online/bottler/letter/application/facade/ReplyLetterFacade.java
+++ src/main/java/online/bottler/letter/application/facade/ReplyLetterFacade.java
@@ -42,8 +42,7 @@ public class ReplyLetterFacade {
 
         Letter letter = letterWithKeywordsUseCase.getLetter(replyLetterCommand.letterId());
         ReplyLetter replyLetter = replyLetterUseCase.create(replyLetterCommand, letter.getUserId(), letter.getTitle());
-        letterBoxUseCase.createForReplyLetter(replyLetter.getLetterId(), replyLetter.getSenderId(),
-                replyLetter.getReceiverId(), replyLetter.getCreatedAt());
+        letterBoxUseCase.archiveLetter(replyLetter);
 
         recentReplyForLetterUseCase.push(replyLetter.getId(), replyLetter.getLabel(), replyLetter.getReceiverId());
 
